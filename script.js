5b6e833267fb397e65b183c44af1a164dfba0367
Sylvester Arikhan <88083504+ambabasly@users.noreply.github.com>
2021-08-16 10:06:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number2 - number1;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  if (number1 = 0) {
+    return "Not Allowed";
+  }else {
+    return number1 / number2;
+  } 
+}
